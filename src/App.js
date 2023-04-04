7d896fbe95563447d0f2de39877221242cc7640a
Ian Sampaio <ianosampaio@gmail.com>
2023-04-04 19:40:01 -0300

    add navbar and footer and more links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
+
+import NavBar from './components/layout/NavBar';
+import Container from './components/layout/Container';
+import Footer from './components/layout/Footer';
 
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header>
-        <p>oi</p>
-      </header>
-    </div>
-  );
+    <Router>
+      <NavBar/>
+      <Switch>
+        <Container customClass="min-height">
+          <Route exact path='/'>
+            <Home/>
+          </Route>
+          <Route exact path='/projects'>
+            <Projects/>
+          </Route>
+          <Route exact path='/company'>
+            <Company/>
+          </Route>
+          <Route exact path='/contact'>
+            <Contact/>
+          </Route>
+          <Route exact path='/newproject'>
+            <NewProject/>
+          </Route>
+        </Container>
+      </Switch>
+      <Footer/>
+    </Router>
+  )
 }
-
-export default App;
