ae03b3f5fd889aa3dde6163a503f3c72e9571419
Ian Sampaio <ianosampaio@gmail.com>
2023-04-10 20:37:21 -0300

    add dashboard

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,4 +1,6 @@
 import Message from "../layout/Message"
+import Container from '../layout/Container'
+import LinkButton from "../layout/LinkButton"
 import styles from './Projects.module.css'
 import { useLocation } from "react-router-dom"
 
@@ -11,9 +13,15 @@ export default function Project() {
     }
 
     return(
-        <>
-            <h1>Meus projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto"/>
+            </div>
             {message && <Message type="seccess" msg={message} />}
-        </>
+            <Container customClass="start">
+                <p>projetos...</p>
+            </Container>
+        </div>
     )
 }
