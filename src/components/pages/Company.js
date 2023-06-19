ccbc09baff533c2ce47ff49f6da768a1740ee233
Ian Sampaio <ianosampaio@gmail.com>
2023-06-19 19:18:59 -0300

    add company and contact

--- src/components/pages/Company.js
+++ src/components/pages/Company.js
@@ -1,7 +1,18 @@
+import styles from './Company.module.css'
+
 export default function Company() {
     return(
         <>
-        <p>oi</p>
+            <div className={styles.about}>
+                <h1>Sobre nós</h1>
+                <p>
+                    A Empresa Costs é uma empresa especializada em fornecer serviços excepcionais para outras empresas. Com nosso eficiente gerenciador de projetos, ajudamos nossos clientes a alcançarem o sucesso em suas empreitadas. Nossa equipe altamente qualificada e experiente está comprometida em oferecer soluções personalizadas que atendam às necessidades específicas de cada cliente.<br/><br/>
+                    Através do nosso gerenciador de projetos, oferecemos uma plataforma intuitiva e abrangente que permite um acompanhamento detalhado de todas as fases do projeto. Desde o planejamento até a execução e entrega final, nossos clientes têm acesso a recursos avançados de organização, colaboração e comunicação. <br/><br/>
+                    Além disso, nossa empresa se destaca pela pontualidade e qualidade de entrega. Trabalhamos com prazos apertados e nos esforçamos para superar as expectativas de nossos clientes em todos os projetos que realizamos. Nossa equipe é altamente comprometida e dedicada a garantir que cada projeto seja concluído com excelência. <br/><br/>
+                    Valorizamos a parceria e o relacionamento de longo prazo com nossos clientes. Estamos sempre disponíveis para ouvir suas necessidades, oferecer suporte e ajustar nossa abordagem para garantir sua satisfação contínua.
+                    Com a Empresa Costs como sua parceira de serviços, você pode ter a certeza de que seus projetos estão em mãos competentes. Entre em contato conosco hoje mesmo e descubra como podemos ajudar sua empresa a alcançar seus objetivos de forma eficiente e eficaz.
+                </p>
+            </div>
         </>
     )
 }
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,3 +1,34 @@
+import Input from '../form/input'
+import SubmitButton from '../form/SubmitButton'
+import styles from './Contact.module.css'
+
 export default function Contact() {
-    return <h1>BALÃO GORDINHO TIPO MITICO JOVEM</h1>
+    return(
+        <div  className={styles.form}>
+
+                <h1>Entre em contato</h1>
+
+                <form>
+                    <Input
+                    text='Nome:'
+                    type='text'
+                    placeholder='Digite seu nome'
+                    />
+
+                    <Input
+                    text='Email:'
+                    type='text'
+                    placeholder='Digite seu email'
+                    />
+
+                    <Input className={styles.mensagem}
+                    text='Mensagem:'
+                    type='text'
+                    placeholder='Escreva uma mensagem'
+                    />
+
+                    <SubmitButton text='Enviar'/>
+                </form>
+        </div>
+    )
 }
